69e853ca763aabce23d422e378a1488edcad70d9
Michael Carr <mcarr2468@gmail.com>
2022-02-22 14:52:45 -0600

    got searchbar dropdown working

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import styled from "styled-components"
 import { CgSearch } from "react-icons/cg"
+import { usePostContext } from '../context/post_context';
+import { Link } from "react-router-dom";
 
 const Search = () => {
   const [userInput, setUserInput] = React.useState("")
+  const [userDataState, setUserDataState] = React.useState([])
+  const { usersData } = usePostContext()
+
   React.useEffect(() => {
-    if(userInput.length > 1) {
-      /*send request to get the user data
-      the corresponds w/ the search term*/
-      console.log(userInput)
+    if(userInput.length >= 1) {
+      let maxAmtOfUsers = 8;
+      let filterUsers = usersData.filter((user) => user.username.includes(userInput))
+      let usersLimited = filterUsers.slice(0, maxAmtOfUsers)
+      setUserDataState(usersLimited)
     }
   }, [userInput])
 
@@ -21,32 +27,23 @@ const Search = () => {
                 <input className="search-input"type="text" placeholder="Search" onChange={(e) => setUserInput(e.target.value)}/>
             </label>
         </div>
-        <div className="search-users-container show-search-users">
-            <div className="triangle-2"></div>
-            <ul className="search-users">
-              {/*make sure to cap the name length at 15 characters long*/}
-                <li className="search-user">
-                    <a className="search-user__link" href="/">
-                        <h2 className="search-user__name">users name</h2>
-                    </a>
-                </li>
-                <li className="search-user">
-                    <a className="search-user__link" href="/">
-                        <h2 className="search-user__name">users name</h2>
-                    </a>
-                </li>
+        <div className={userInput.length >= 1 ? "search-users-container show-search-users" : "search-users-container"}>
+        {userInput.length >= 1 ? (
+          <ul className="search-users">
+            {userDataState?.length > 1 ? userDataState.map((user) => {
+              const { uid, username } = user;
+              let usernameCharMax = 15;
 
-                <li className="search-user">
-                    <a className="search-user__link" href="/">
-                        <h2 className="search-user__name">max amt of char...</h2>
-                    </a>
+              return (
+                <li className="search-user" key={uid}>
+                  <Link className="search-user__link" to={`/${username}`}>
+                    <h2 className="search-user__name">{username.length < usernameCharMax ? username : `${username.slice(0, usernameCharMax)}...`}</h2>
+                  </Link>
                 </li>
-                <li className="search-user">
-                    <a className="search-user__link" href="/">
-                        <h2 className="search-user__name">users</h2>
-                    </a>
-                </li>
-            </ul>
+              )
+            }) : <p className="no-result-text">No Results found.</p>}
+          </ul>
+            ) : ""}
         </div>
     </Wrapper>
   )
@@ -94,15 +91,11 @@ const Wrapper = styled.section`
     border-left: 10px solid transparent;
     border-right: 10px solid transparent;
     border-bottom: 10px solid #fff;
-    top:-10px;
-    left:50%;
-    transform:translate(-50%);
-    z-index:999;
+    top:67px;
+    left:35.5%;
+    transform:translate(-35.5%);
+    z-index:10;
   }
-
-
-  
-
 
   .search-users-container {
     visibility: hidden;
@@ -111,9 +104,10 @@ const Wrapper = styled.section`
     min-width: 10rem;
     width:15rem;
     height:auto;
+    max-height:20rem;
     position: absolute;
     transition: all 0.5s ease;
-    margin-top: 1rem;
+    margin-top: 1.5rem;
     left:-100px;
     background:#fff;
     border-radius:5px;
@@ -121,7 +115,19 @@ const Wrapper = styled.section`
     z-index:-10;
     left:34%;
     transform:translate(-34%);
-    
+  }
+
+  .search-users-container:after {
+    position: absolute;
+    left: 50%;
+    margin-left: -20px;
+    top: -15px;
+    width: 0;
+    height: 0;
+    content:'';
+    border-left: 15px solid transparent;
+    border-right: 15px solid transparent;
+    border-bottom: 15px solid #fff;
   }
 
   @media screen and (max-width:1400px) {
@@ -152,17 +158,16 @@ const Wrapper = styled.section`
     opacity: 1;
     display: block;
     z-index:10;
-    
   }
 
 
   .search-users {
     display:flex;
     flex-direction:column;
-    justify-content:center;
-    align-items:center;
-    overflow:auto;
-    text-align:left;
+    justify-content:flex-start;
+    align-items:flex-start;
+    height:auto;
+    max-height:100%;
   }
 
 
@@ -171,6 +176,10 @@ const Wrapper = styled.section`
     font-size:0.85rem;
     border-radius:inherit;
     width:auto;
+  }
+
+  .search-user:hover {
+    color:#000;
   }
 
   .search-user:first-of-type {
@@ -183,5 +192,12 @@ const Wrapper = styled.section`
 
   .search-user__name {
     font-weight:400;
+  }
+
+
+  .no-result-text {
+    width:100%;
+    padding:0.5rem;
+    text-align:center;
   }
 `
