cd70d53caa95f6ba9b3ff29ccef1fa98831a74a3
Michael Carr <mcarr2468@gmail.com>
2022-03-30 19:40:42 -0500

    refactored the styles to be cleaner and more organized and reworked how some of the css worked

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -21,6 +21,7 @@ const SinglePost = () => {
   const [showDeleteButton, setShowDeleteButton] = useState(true)
   const [showCommentEl, setShowCommentEl] = useState(true)
   let descriptionText = "Unlike normal “Chirashi,” where the fish is normally sliced up more like sashimi, the fish in Bara Chirashi, however, is diced up into smaller pieces, making each a perfect size to enjoy in one bite. We hope that you’ll enjoy Chef Kawabe’s own upgraded version of this classic dish."
+  let testPostImg = "https://images.unsplash.com/photo-1643304842006-44079673c553?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
 
   const {
     loggedInUid,
@@ -62,75 +63,142 @@ const SinglePost = () => {
       <Navbar/>
 
       <section className="post">
-        <div className="post-content">
-          <img className="post-img" src="https://images.unsplash.com/photo-1643304842006-44079673c553?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="post"/>
+        <div className="post__img-container">
+          <img className="post__img" src={testPostImg} alt="post"/>
         </div>
 
-        <div className="post__info">
-          <header className="header">
-            <Link className="header__info" to="/the users profile">
-              <img className="profile-img" src={defaultImage} alt="profile"/>
-            </Link>
-            <div className="post__info__data">
-              <Link className="header__info" to="/the users profile">
-                <h2 className="username">Username here</h2>
-              </Link>
-
-              <div className="post__info__data__dot">•</div>
-              {/*blue follow btn text or black following text */}
-              <div className="post__is-following">Following</div>
-            </div>
-            <button className="delete-post-btn" onClick={() => openAreYouSureModal()}>
-              <BiDotsHorizontalRounded className="delete-post-icon"/>
+        <div className="post__details">
+          {/* header */}
+          <header className="post__header">
+            <img className="post__header__img" src={defaultImage} alt=""/>
+            <h2 className="post__header__username">username here</h2>
+            <button className="post__header__action-btn">
+              <BiDotsHorizontalRounded className="post__details__icon horizontal-dots-icon"/>
             </button>
           </header>
+          {/* comments */}
+          <div className="post__comments-outer">
+            <div className="post__comments-inner">
+              <div className="post__comments">
+                <ul className="post__comments__list">
+                  {/*first item is the description of the post*/}
+                  <li className="comments__list__comment">
+                    <Link className="comment__profile-img-container" to="/the users profile">
+                      <img className="comment__profile-img" src={defaultImage} alt="profile"/>
+                    </Link>
 
-          {/* <hr className="divider"/> */}
+                    <div className="comment__info">
+                      <Link className="comment__info__link" to="/the users profile">
+                        <h2 className="comment__info__username">Username here</h2>
+                      </Link>
 
-          <section className="post__comments">
-            <ul className="comments_list">
-              {/*this is the post description */}
-              <li className="comment">
-                <Link className="header__info" to="/the users profile">
-                  <img className="profile-img" src={defaultImage} alt="profile"/>
-                  <h2 className="username">Username here</h2>
-                </Link>
-                <p>this is the description</p>
-              </li>
-              {/*the rest of this are user comments*/}
-              <li className="comment">
-                <Link className="header__info" to="/the users profile">
-                  <img className="profile-img" src={defaultImage} alt="profile"/>
-                  <h2 className="username">Username here</h2>
-                </Link>
-                <p>this is a comment from a user</p>
-              </li>
-            </ul>
-          </section>
+                      <p className="comment__info__text">this is the description</p>
+                    </div>
+                  </li>
+                  {/*every other item is a comment*/}
+                  <li className="comments__list__comment">
+                    <Link className="comment__profile-img-container" to="/the users profile">
+                      <img className="comment__profile-img" src={defaultImage} alt="profile"/>
+                    </Link>
 
-          {/* <hr className="divider"/> */}
+                    <div className="comment__info">
+                      <Link className="comment__info__link" to="/the users profile">
+                        <h2 className="comment__info__username">Username here</h2>
+                      </Link>
 
-          <div className="post-details">
-            <ul className="post-details__items">
-              <li className="post-details__item">
-                <button className="post-btn like-btn">{likedPost ? <AiFillHeart className="like-btn--liked"/> : <AiOutlineHeart/>}</button>
-              </li>
+                      <p className="comment__info__text">this is a comment.this is a comment.this is a comment.this is a comment.this is a comment.</p>
+                    </div>
+                  </li>
+                  <li className="comments__list__comment">
+                    <Link className="comment__profile-img-container" to="/the users profile">
+                      <img className="comment__profile-img" src={defaultImage} alt="profile"/>
+                    </Link>
 
-              <li className="post-details__item">
-                <button className="post-btn like-btn" ><FiMessageSquare/></button>
-              </li>
-            </ul>
-            <div className="post-likes">9,037 likes</div>
-            <div className="date-posted">1 HOUR AGO</div>
+                    <div className="comment__info">
+                      <Link className="comment__info__link" to="/the users profile">
+                        <h2 className="comment__info__username">Username here</h2>
+                      </Link>
+
+                      <p className="comment__info__text">this is a comment.this is a comment.this is a comment.this is a comment.this is a comment.</p>
+                    </div>
+                  </li>
+                  <li className="comments__list__comment">
+                    <Link className="comment__profile-img-container" to="/the users profile">
+                      <img className="comment__profile-img" src={defaultImage} alt="profile"/>
+                    </Link>
+
+                    <div className="comment__info">
+                      <Link className="comment__info__link" to="/the users profile">
+                        <h2 className="comment__info__username">Username here</h2>
+                      </Link>
+
+                      <p className="comment__info__text">this is a comment.this is a comment.this is a comment.this is a comment.</p>
+                    </div>
+                  </li>
+                  <li className="comments__list__comment">
+                    <Link className="comment__profile-img-container" to="/the users profile">
+                      <img className="comment__profile-img" src={defaultImage} alt="profile"/>
+                    </Link>
+
+                    <div className="comment__info">
+                      <Link className="comment__info__link" to="/the users profile">
+                        <h2 className="comment__info__username">Username here</h2>
+                      </Link>
+
+                      <p className="comment__info__text">this is a comment.this is a comment.this is a comment.this is a comment.this is a comment.</p>
+                    </div>
+                  </li>
+                  <li className="comments__list__comment">
+                    <Link className="comment__profile-img-container" to="/the users profile">
+                      <img className="comment__profile-img" src={defaultImage} alt="profile"/>
+                    </Link>
+
+                    <div className="comment__info">
+                      <Link className="comment__info__link" to="/the users profile">
+                        <h2 className="comment__info__username">Username here</h2>
+                      </Link>
+
+                      <p className="comment__info__text">this is a comment.this is a comment.this is a comment.this is a comment.this is a comment.</p>
+                    </div>
+                  </li>
+                  <li className="comments__list__comment">
+                    <Link className="comment__profile-img-container" to="/the users profile">
+                      <img className="comment__profile-img" src={defaultImage} alt="profile"/>
+                    </Link>
+
+                    <div className="comment__info">
+                      <Link className="comment__info__link" to="/the users profile">
+                        <h2 className="comment__info__username">Username here</h2>
+                      </Link>
+
+                      <p className="comment__info__text">this is a comment.</p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          {/* details */}
+          <div className="post__info__details">
+            <div className="post__info__details__icons">
+              <button className="post_info__like-btn">
+                <AiOutlineHeart className="post__info__icon post__heart-icon"/>
+              </button>
+              <button className="post-info__comment-btn">
+                <FiMessageSquare className="post__info__icon post__msg-icon"/>
+              </button>
+            </div>
+            <div className="post__info__likes">19,286 likes</div>
+            <div className="post__info__date">1 DAY</div>
           </div>
 
-          {/* <hr className="divider"/> */}
-          
-          <div className="post-a-comment">
+          {/* pos-a-comment */}
+          <div className="post__info__post-a-comment">
             <label className="post-a-comment__label">
-              <input className="post-a-comment__input" type="text" placeholder="Add a comment..."/>
+              <input className="post-a-comment__input" placeholder="Add a comment..."/>
             </label>
-            <button className="post-a-comment-btn">Post</button>
+
+            <button className="post-a-comment__btn">Post</button>
           </div>
 
         </div>
@@ -143,220 +211,245 @@ export default SinglePost;
 
 
 const Wrapper = styled.div`
-  width:99vw;
+  width:100vw;
+  position:relative;
 
 
-  .post {
-    border:1px solid var(--gray-db);
-    border-radius:2px;
-    background:#fff;
-    margin:1.5rem 0;
 
-    width:60rem;
-    height:40rem;
-    position:relative;
-    left:50%;
-    transform:translateX(-50%);
-    display:grid;
-    grid-template-columns:1fr 0.6fr;
+  .img-temp  {
+    width:100px;
   }
 
-  .post__info {
-    display:grid;
-    grid-template-rows:auto 1fr auto auto;    
-  }
-
-  .post__info__data {
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    margin-right:auto;
-  }
-
-  .post__info__data__dot {
-    margin:0 0.5rem ;
-  }
-
-  .header {
-    display:flex;
-    justify-content:space-between;
-    align-items:center;
-    margin:0.75rem 0.75rem;
-  }
-
-  .header__info {
-    display:flex;
-    justify-content:center;
-    align-items:center;
-  }
-
-  .profile-img {
-    width:27px;
-    height:27px;
-    border-radius:100px;
-    border:none;
-    cursor:pointer;
-    margin-right:1rem;
-    object-fit:cover;
-  }
-
-  .username,
-  .post__is-following {
-    font-weight:500;
-    font-size:1rem;
-    color:#000;
-  }
-
-  .delete-post-btn {
-    position:relative;
-    background:none;
-    border:none;
-    font-size:1.25rem;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    top:2px;
-    cursor:pointer;
-  }
-
-
-  .post-img {
-    width:100%;
-    height:auto;
-    max-height:40rem;
-    object-fit:cover;
-  }
-
-  .post-details {
-    padding:1rem;
-    border-bottom:1px solid var(--gray-divider);
-  }
-
-  .post-details__items {
-    display:flex;
-    align-items:center;
-  }
-
-  .post-details__item {
-    border:none;
-    background:none;
-    font-size:1.75rem;
-  }
-
-  .divider {
-    display: block; height: 1px;
-    border: 0; border-top: 1px solid var(--gray-divider);
-    margin:0; padding: 0;
-  }
-
-
-  .comments_list {
-    padding:1rem 0;
-  }
-
-  .comment {
-    display:flex;
-    align-items:center;
-    padding:0 0.75rem;
-  }
-
-  .post-btn {
-    background:none;
-    border:none;
-    font-size:1.65rem;
-    cursor:pointer;
-  }
-
-  .like-btn {
-    margin-right:0.5rem;
-    cursor:pointer;
-  }
-
-  .like-btn--liked {
-    color:#ED4956;
-  }
-
-  .post-likes {
-    margin:0.35rem 0 0.25rem 0;
-    font-weight:500;
-  }
-
-  .more-description-btn,
-  .date-posted,
-  .view-comments-btn {
-    border:none;
-    background:none;
-    color:gray;
-    display:block;
-  }
-
-  .more-description-btn {
-    display:inline;
-    cursor:pointer;
-  }
-
-  .view-comments-btn  {
-    margin:0.25rem 0;
-    font-size:1rem;
-  }
-
-  .date-posted {
-    font-size:0.75rem;
-    margin-top:0.25rem;
-  }
-
-  .post-a-comment {
-    display:flex;
-    padding:1rem 1rem;
-    justify-content:space-between;
-    align-items:center;
-  }
-
-  .post-a-comment__label {
-    height:inherit;
-    display:block;
-  }
-
-  .post-a-comment__input {
-    height:100%;
-    border:none;
-    outline:none;
-  }
-
-  .post-a-comment-btn {
-    border:none;
-    background:none;
-    color:var(--login-btn-bg);
-  }
-
-  .post-a-comment-btn--validated {
-    color:var(--login-btn-bg-validated);
-    cursor:pointer;
-  }
-
-
-
-
-
-  .post__comments {
-    border-top:1px solid var(--gray-divider);
-    border-bottom:1px solid var(--gray-divider);
-  }
-
-
-
-
-
-  @media screen and (max-width:700px) {
+  // post 
     .post {
-      min-width:20rem;
-      width:100%;
+      background:#fff;
+      width:60rem;
+      height:40rem;
+      position:relative;
+      top:5rem;
+      left:50%;
+      transform:translateX(-50%);
+      border:1px solid var(--gray-db);
+      display:grid;
+      grid-template-columns:1fr 0.5fr;
     }
 
-    .post-img {
+
+
+
+
+    // post img
+      .post__img-container {
+        position:relative;
+        width:max-content;
+        height:39.9rem;
+        left:50%;
+        transform:translate(-50%);
+      }
+
+      .post__img {
+        width:auto;
+        height:100%;
+      }
+
+    
+
+
+
+    // post details
+      .post__details {
+        border-left:1px solid var(--gray-db);
+        display:grid;
+        grid-template-rows:auto 25rem auto auto;
+      }
+
+    
+
+
+    // header
+     .post__header {
+       display:flex;
+       align-items:center;
+       padding:1rem;
+     }
+     .post__header__img {
+       width:30px;
+       height:30px;
+       border-radius:100px;
+       border:none;
+       cursor:pointer;
+       margin-right:1rem;
+       object-fit:cover;
+     }
+
+     .post__header__username {
+      font-weight:500;
+      font-size:1rem;
+      color:#000;
+     }
+
+     .post__header__action-btn {
+      position:relative;
+      background:none;
+      border:none;
+      font-size:1.25rem;
+      display:flex;
+      justify-content:center;
+      align-items:center;
+      top:2px;
+      cursor:pointer;
+      margin-left:auto;
+     }
+
+
+
+
+     // comments
+    .post__comments-outer,
+    .post__comments {
+      width:auto;
+      height:25rem;
+    }
+
+    .post__comments-outer {
+      border-top:1px solid var(--gray-divider);
+      border-bottom:1px solid var(--gray-divider);
+      position:relative;
+      overflow:hidden;
+    }
+
+    .post__comments-inner {
+      position:absolute;
+      left:0;
+      overflow-x:hidden;
+      overflow-y:scroll;
+    }
+
+    .post__comments-inner::-webkit-scrollbar {
+      display:none;
+    }
+
+    // ul
+    .post__comments__list {
+      padding:1rem;
+    }
+    // li
+    .comments__list__comment {
+      padding:0.5rem 0rem;
+      display:grid;
+      grid-template-columns:auto 1fr;
+    }
+
+    .comment__profile-img {
+      width:30px;
+      height:30px;
+      border-radius:100px;
+      border:none;
+      cursor:pointer;
+      margin-right:1rem;
+      object-fit:cover;
+    }
+
+
+    .comment__info__username {
+      font-weight:500;
+      font-size:1rem;
+      color:#000;
+      margin-right:0.5rem;
+      display:inline;
+    }
+
+    .comment__info__text {
+      display:inline;
+    }
+
+
+
+
+
+    // post info
+    .post__info__details {
+      display:flex;
+      flex-direction:column;
+      padding:1rem;
+      position:relative;
+    }
+
+
+    .post_info__like-btn,
+    .post-info__comment-btn {
+      border:none;
+      background:none;
+      font-size:2rem;
+      cursor:pointer;
+    }
+
+    .post_info__like-btn {
+
+    }
+
+    .post_info__like-btn--liked {
+      color:#ED4956;
+    }
+
+
+    .post-info__comment-btn {
+      margin-left:0.5rem;
+    }
+
+    .post__info__likes {
+      margin:0.25rem 0;
+      font-weight:500;
+    }
+
+    .post__info__date {
+      font-size:0.85rem;
+      color:gray;
+    }
+
+
+
+
+
+    // post a comment
+    .post__info__post-a-comment {
+      position:relative;
+      border-top:1px solid var(--gray-divider);
+      // align-items:center;
+      padding:0.5rem;
+
+      width:100%;
+      display:flex;
+    }
+
+
+
+    .post-a-comment__label{
+      background:red;
+      flex:1;
+    }
+
+    .post-a-comment__input {
       width:100%;
       height:100%;
-      object-fit: cover;
+      border:none;
+      outline:none;
     }
-  }
+    
+    .post-a-comment__input::placeholder {
+
+    }
+    
+    .post-a-comment__btn {
+      border:none;
+      background:none;
+      color:var(--login-btn-bg);
+      margin-left:0.25rem;
+    }
+
+    .post-a-comment__btn--validated {
+      color:var(--login-btn-bg-validated);
+      cursor:pointer;
+    }
+
 `
